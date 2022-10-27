e83963f8d7f3dbe7c3ac98059c510aecdc589fae
Danang Arif Rahmanda <darifrahmanda@gmail.com>
2022-10-28 03:42:53 +0700

    static template

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderStore />
+    <HeroStore />
+    <ProductStore />
+    <InstaListStore />
+    <PartnerStore />
+    <FooterStore />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderStore from "@/components/HeaderStore.vue";
+import HeroStore from "@/components/HeroStore.vue";
+import ProductStore from "@/components/ProductStore.vue";
+import InstaListStore from "@/components/InstaListStore.vue";
+import PartnerStore from "@/components/PartnerStore.vue";
+import FooterStore from "@/components/FooterStore.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HeaderStore,
+    HeroStore,
+    ProductStore,
+    InstaListStore,
+    PartnerStore,
+    FooterStore,
+  },
+};
+</script>
